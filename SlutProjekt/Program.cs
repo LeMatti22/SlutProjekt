82b52c73f836c36eb7337f68939d0285fd67a0e8
Matti Thorson <thorsson.mattilo@elev.ga.ntig.se>
2022-05-09 20:03:11 +0200

    Rain

--- SlutProjekt/Program.cs
+++ SlutProjekt/Program.cs
@@ -1,3 +1,43 @@
 ﻿using System;
 using Raylib_cs;
 
+
+Rain[] rain =  new Rain[1000];
+
+for(int i = 0; i < rain.Length; i++)
+{
+    rain[i] = new Rain();
+
+    rain[i].lol(i*2, -20);
+
+}
+
+Raylib.InitWindow(1280, 720, "SlutProjekt");
+while(!Raylib.WindowShouldClose()){
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.BLACK);
+
+    for(int i = 0; i < rain.Length; i++)
+{
+    Raylib.DrawRectangleRec(rain[(i)].raindrop, Color.BLUE);
+}
+
+    Raylib.EndDrawing();
+}
+
+class Rain
+{
+    public Rectangle raindrop = new Rectangle();
+    public void lol(int xOffset, int yOffset)
+    {
+    raindrop = new Rectangle(xOffset, yOffset, 1, 3);
+    }
+
+} 
+
+void rainDownfall()
+{
+    while()
+}
+
+
